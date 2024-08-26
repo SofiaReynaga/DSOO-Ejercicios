459bfae845cfeedcc8dc73a3c0dcdae652de586a
sofi3224 <sofia.reynaga.et7@gmail.com>
2024-08-26 18:04:03 -0300

    Punto2: Concatenacion de dos variables

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -12,6 +12,13 @@ def main():
     #Se muestra por pantalla el resultado
     print(f"El resultado de la suma es: {suma}")
 
+    #Ejercicio2
+
+    cadena1="Hola"
+    cadena2=" Mundo"
+    # se conectan las dos variables que declaramos, mostrando "Hola Mundo"
+    print(cadena1 + cadena2)
+
 
 if __name__ == '__main__':
     main()
