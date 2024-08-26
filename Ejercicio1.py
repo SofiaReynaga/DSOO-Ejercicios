0035f2fbaf644fc8d46ff04130689839a7f76560
sofi3224 <sofia.reynaga.et7@gmail.com>
2024-08-26 18:02:38 -0300

    Punto1: Operacion de dos variables

---
+++ Ejercicio1.py
@@ -0,0 +1,17 @@
+
+def main():
+    pass
+#Ejercicio1
+
+    num1=10  #Se inicializa las varaibles
+    num2=20
+
+    #Se hace la operacion suma, sumando las dos variables declaradas anteriormente
+    suma=num1+num2
+
+    #Se muestra por pantalla el resultado
+    print(f"El resultado de la suma es: {suma}")
+
+
+if __name__ == '__main__':
+    main()
